b36ed69797e1e1e8dfd9bad2a25f592999611c71
ro-mak <eee127@yandex.ru>
2018-09-20 23:47:16 +0300

    Initial commit

---
+++ desktop/src/ru/liveedu/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.liveedu.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.liveedu.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
